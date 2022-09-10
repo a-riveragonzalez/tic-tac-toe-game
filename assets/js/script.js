59ee5bc6d0054cee7d8231446d291d7ef7941acd
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-09 23:46:33 -0700

    added function to add X when space is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,37 @@ let loseCount = 0;
 
 
 // ********************* Functions ********************//
-function clickXO (event) { 
-    console.log(event);
-    
+
+// if click, add x
+// if click, add o 
+function clickXO (elementClicked) {
+    if (elementClicked.textContent===""){
+        $(elementClicked).text("X");
+    } else{
+        console.log("already taken")
+    }
+
+    // $(elementClicked).text("X");
+    // console.log(elementClicked.textContent);
+}
+
+
+function checkWin() {
+    // if three in a row, win message
+    // else continue game 
+}
+
+function playGame (event) { 
+    const elementClicked = event.currentTarget.firstElementChild;
+
+    clickXO(elementClicked);
+    // if (elementClicked)
+    // $(elementClicked).text("X");
+    // console.log(elementClicked.textContent);
+
+    // clickx or clicko
+    // check if win 
  }
 
 // ****************** Calling Functions ******************//
-container.on("click", "div", clickXO)
+container.on("click", "div", playGame)
